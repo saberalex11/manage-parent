0a137b020d63699a163c2e53c3546ca0fff40fe1
lee <867244890@qq.com>
2018-01-18 12:00:00 +0800

    Initial commit

---
+++ manage-platform/src/test/java/com/lee/manageplatform/ManagePlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lee.manageplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagePlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
